eb0ba6cad3e2bfd6de4dd2272edf011e54f0117c
williamchen <williamchenwl880@gmail.com>
2018-01-31 13:31:51 +0800

    modified

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,6 +1,11 @@
-clear
-
 answer=$(ls | wc -l) #print the new line counts to calculate how many files
+
+number=0
+
+function correct {
+    echo "you guess the right answer!"
+    echo "you used times" $number
+}
 
 echo "This is a game for you to guess how many files there are in the directory"
 echo "Hidden files are not counted, now tell me the number: "
@@ -11,11 +16,13 @@ do
     if [[ $input -gt $answer ]]
     then
         echo "You guess higher than the correct answer!!"
+        number=$number+1
     else
         echo "You guess lowert than the correct answer!!"
+        number=$number+1
     fi
     echo "Tell me you guess again: "
     read input
 done
 
-echo "Exactly, that's the answer"
+correct
